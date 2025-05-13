930a4a79109517dd1a925291f9d2a590b2c6dd9f
Syed umair <mianumairhayatkakakhel42@gmail.com>
2025-05-13 01:16:49 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
 window.onload = function () {
     alert("Welcome to ConnectVerse!");
 };
+
+
+// Only allow access if logged in
+if (localStorage.getItem("isLoggedIn") !== "true") {
+    window.location.href = "login.html";
+} else {
+    alert("Welcome to ConnectVerse!");
+}
